d30080a293bf7f1663c87935fa72495347361f86
kamrul565 <as9172191@gmail.com>
2024-02-19 13:11:01 +0600

    cuppon offer

--- script/script.js
+++ script/script.js
@@ -1,22 +1,28 @@
 
 let booked = 0;
+let costs = 0;
 
 const seats = document.querySelectorAll('#seats');
 
-for(let seat of seats) {
-    
-    seat.addEventListener('click', function(e) {
+for (let seat of seats) {
+
+    seat.addEventListener('click', function (e) {
 
         booked += 1;
 
-        if(booked > 4) {
+        if (booked > 4) {
             alert('Can not book more than 4');
             return;
         }
 
-        if(booked == 4) {
+        if (booked == 4) {
             let apply = document.getElementById('apply');
-            apply.removeAttribute('disabled');
+            //apply.removeAttribute('disabled');
+            apply.disabled = false;
+        }
+        else {
+            let apply = document.getElementById('apply');
+            apply.disabled = true;
         }
 
         //background change
@@ -26,15 +32,57 @@ for(let seat of seats) {
 
         let seatNo = e.target.innerText;
         addSeats(seatNo);
-        
+
 
         let avaliableSeats = getValue('available-seats');
         setValue('available-seats', avaliableSeats - 1);
 
         setValue('selected-seats', booked);
 
+        costs += 550;
+        setValue('total-cost', costs);
+        setValue('grand-total', costs);
+
     })
 }
+
+document.getElementById('apply').addEventListener('click', function(e) {
+    const cupponOffer = document.getElementById('cuppon-offer');
+    let val  = cupponOffer.value;
+
+    let arr = val.split(' ');
+
+
+    let offer = arr[0];
+    offer = offer.toLowerCase();
+    if(arr.length == 2) offer += arr[1];
+
+    if(offer == 'new15') {
+        let discount = costs - (costs * 0.15);
+        setValue('grand-total', discount);
+        hidden('cupon-div');
+    }
+    else if(offer == 'couple20') {
+        let discount = costs - (costs * 0.2);
+        setValue('grand-total', discount);
+        hidden('cupon-div');
+    }
+    else {
+        alert('Please enter valid Cupon!!');
+    }
+    
+    //console.log(offer);
+
+
+    cupponOffer.value = '';
+
+    
+})
+
+
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+})
 
 function getValue(id) {
     let val = document.getElementById(id);
@@ -45,6 +93,11 @@ function getValue(id) {
 function setValue(id, val) {
     let x = document.getElementById(id);
     x.innerText = val;
+}
+
+function hidden(id) {
+    let x = document.getElementById(id);
+    x.classList.add('hidden');
 }
 
 function addSeats(seatNo) {
@@ -66,3 +119,13 @@ function addSeats(seatNo) {
 
     document.getElementById('selected-seat-list').appendChild(seat);
 }
+
+
+
+
+let apply = document.getElementById('apply');
+apply.disabled = true;
+
+let nextBtn = document.getElementById('next-btn');
+nextBtn.disabled = true;
+
